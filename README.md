40098984e2bcb5a045bbf4af57accfd40e48557d
Denis Vargas Rivero <denis.vargasrivero@outlook.com>
2022-08-29 16:33:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aframe-BlankExperience-Template
+ Una Template en blanco para experiencias construidas en Aframe
---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(PORT, ()=>{
+    console.log(`App running in port:${PORT}`);
+});
